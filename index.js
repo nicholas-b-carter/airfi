c9408f831cac5ea17ab8b8c96d34901a6c97c032
Akshay Kulkarni <akshaykulkarni.2104@gmail.com>
2016-12-31 19:13:47 +0530

    added real airports to countries

--- index.js
+++ index.js
@@ -1238,30 +1238,20 @@ intents.matches('findPassword',function(session,args){
             });
             session.send("We found " + airports.length + " airports in " + country.entity);
 
+            var attchments = [];
+
+            for (var i=0;i<airports.length;i++){
+                var attachment = new builder.HeroCard(session)
+                                    .title(airports[i]['name'])
+                                    .subtitle(airports[i]['city'])
+                                    .buttons([
+                                        builder.CardAction.imBack(session, airports[i]['name'], "Select")
+                                    ]);
+            }
+
             var msg = new builder.Message(session)
                             .attachmentLayout(builder.AttachmentLayout.carousel)
-                            .attachments([
-                                new builder.HeroCard(session)
-                                    .title("Space Needle")
-                                    .subtitle("The Space Needle is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
-                                    .images([
-                                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                                            .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/800px-Seattlenighttimequeenanne.jpg")),
-                                    ])
-                                    .buttons([
-                                        builder.CardAction.imBack(session, "select:100", "Select")
-                                    ]),
-                                new builder.HeroCard(session)
-                                    .title("Pikes Place Market")
-                                    .subtitle("Pike Place Market is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
-                                    .images([
-                                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
-                                            .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/800px-PikePlaceMarket.jpg")),
-                                    ])
-                                    .buttons([
-                                        builder.CardAction.imBack(session, "select:101", "Select")
-                                    ]),
-                            ]);
+                            .attachments(attchments);
                 session.endDialog(msg);
         }
     }
